fb2437cac12f367edd4f4277493627cbdbd98437
cristianoceke <67923297+cristianoceke@users.noreply.github.com>
2023-09-19 23:19:44 -0300

    Desafio de Projeto (Lab) realizado

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,63 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO } // nível do curso
 
-class Usuario
+// data class dos usuários
+data class Usuario(
+    val nome: String, 
+    val idade: Int, 
+    val email: String,
+    val nivel: Nivel
+)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+// data class dos ConteudoEducacional
+data class ConteudoEducacional(
+    var nome: String, 
+    val duracao: Int = 120
+)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+// data class da formação
+data class Formacao(
+    val nome: String, 
+    var conteudos: List<ConteudoEducacional>
+) {
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    // aplicando a função vararg para adicionar mais usuários
+    fun matricular(vararg usuarios: Usuario) {
+        inscritos.addAll(usuarios) 
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    // Módulos
+    val modulo1 = ConteudoEducacional("", 100)
+    val modulo2 = ConteudoEducacional("Introdução a linguagem Kotlin", 110)
+    
+     // Formacão
+    val formacao = Formacao("Bootcamp da Dio", listOf(modulo1,modulo2))
+    
+    // Alunos
+    val aluno1 = Usuario("Cristiano", 25, "cekeaudio@gmail.com", nivel = Nivel.BASICO)
+    val aluno2 = Usuario("Esdras", 30, "cekeaudio@hotmail.com", nivel = Nivel.AVANCADO)
+    val aluno3 = Usuario("Junior", 35, "cekeaudio@yahoo.com", nivel = Nivel.INTERMEDIARIO)
+    
+    // Formação Matricula
+    formacao.matricular(aluno1, aluno2, aluno3)
+    
+    // Imprimindo lista de inscritos
+    println("Inscritos na formação ${formacao.nome}: ${formacao.inscritos}")
+    println()
+    
+    // Imprimindo nomes, carga horária e email dos alunos matriculados
+    println("Informações dos usuários matriculados na formação ${formacao.nome}:")
+    formacao.inscritos.forEach { usuario ->
+        println("Nome: ${usuario.nome}")
+        println("Email: ${usuario.email}")
+        println("Carga Horária Total: ${formacao.conteudos.sumOf { it.duracao }} minutos")
+        println("Nivel do Curso: ${usuario.nivel}")
+        println("-------------")
+    }
+    
 }
